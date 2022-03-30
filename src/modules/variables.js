e8ab38aedc28cee1e1eafee2fedcdbad9192b9ee
zkr024 <zkr.bm.02403@gmail.com>
2022-03-30 17:50:14 -0500

    Linters warning message

--- src/modules/variables.js
+++ src/modules/variables.js
@@ -1,3 +1,4 @@
 export const list = document.getElementById('list');
 export const addListContainer = document.querySelector('.addListContainer');
 export const clear = document.querySelector('.clearAll');
+export const warning = document.getElementById('warning');
